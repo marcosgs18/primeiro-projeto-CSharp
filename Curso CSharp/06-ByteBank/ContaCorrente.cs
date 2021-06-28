df3b60cc010c0d3fd7d01280514f366a48d81371
marcosgs18 <marcos.gabriel07@hotmail.com>
2021-06-28 11:08:15 -0300

    commit do projeto final

---
+++ Curso CSharp/06-ByteBank/ContaCorrente.cs
@@ -0,0 +1,70 @@
+namespace _06_ByteBank
+
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+        private double _saldo = 100;
+
+        public static int TotalDeContasCriadas { get; private set; }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+
+            TotalDeContasCriadas++;
+        }
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                _saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (valor > _saldo)
+            {
+                return false;
+            }
+            else
+            {
+                _saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+
+        }
+    }
+}
---
+++ Curso CSharp/06-ByteBank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(867, 867938);
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+
+            ContaCorrente contaGabriela = new ContaCorrente(867, 857694);
+
+            Console.WriteLine(contaGabriela.Agencia);
+            Console.WriteLine(contaGabriela.Numero);
+
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+        }
+    }
+}
